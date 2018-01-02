a7f60c5b41bcdc6f4b14cf558e2547e825e6f061
khawkins98 <khawkins98@gmail.com>
2018-01-02 11:49:21 +0000

    Add menu only template

---
+++ page-templates/menu-only.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Template Name: Menu Only
+*/
+/*
+This is used as "hack" to show only the menu -- so it can be iframed or consumed by external sites.
+
+To use, make a new WordPress page and use the "menu only" template.
+*/
+// get_header();
+?>
+
+<?php get_template_part( 'template-parts/featured-image' ); ?>
+
+<div id="menu-only">
+
+<header id="masthead" class="masthead" data-sticky data-options="marginTop: 0">
+  <div class="masthead-inner row">
+    <div class="columns medium-2 small-4 log-column" id="local-title">
+      <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Back to <?php bloginfo( 'name' ); ?> homepage"><img src="https://ebiwd.github.io/pdx-visual-framework/images/logo.png" class="padding-15"/></a></h1>
+    </div>
+    <!-- Desktop Menu-->
+    <div class="columns medium-10 menu-column padding-vertical-30 hide-for-small-only" id="local-nav">
+      <nav id="main-menu" class="navigation" role="navigation">
+        <?php pdxtheme_top_bar_r(); ?>
+        <?php
+        //if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) :
+        ?>
+          <?php
+          //get_template_part( 'template-parts/mobile-top-bar' );
+          ?>
+        <?php
+      // endif;
+      ?>
+      </nav>
+    </div>
+    <!-- Mobile Menu -->
+    <div class="columns small-4 show-for-small-only padding-vertical-15 text-right">
+      <a class="menu-toggle"><i class="icon icon-functional" data-icon="M" data-toggle data-responsive-toggle="expand-menu"></i></a>
+    </div>
+  </div>
+</header>
+
+<?php the_content(); ?>
+
+</div>
